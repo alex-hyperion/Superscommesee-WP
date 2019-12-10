8bf160703895c7f3d86424e83e2a80d04f53cbc1
julia-karpenko <julia.karpenko@catenamedia.com>
2019-12-10 13:29:25 +0200

    added footer and styles

--- wp-content/themes/superscommesse/index.php
+++ wp-content/themes/superscommesse/index.php
@@ -10,7 +10,61 @@
 
 get_header();
 ?>
-
+	<footer class="footer" id="footer"><a class="to-top" href="#" id="js_to_top">Torna a inizio pagina</a>
+		<div class="footer-wrap">
+			<ul class="copyright-list">
+				<li><a href="#">Condizioni generali</a></li>
+				<li><a href="#">Privacy e cookie policy</a></li>
+			</ul>
+			<div class="info-wrap">
+				<div class="warning-text">
+					<p>il gioco è vietato ai minori di anni 18 - può causare dipendenza</p>
+					<ul class="icon-list">
+						<li><img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/age.svg" alt="18+" width="40"></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/legal.svg" alt="gioco legale e responsabile" width="70"></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/aams.svg" alt="aams" width="49"></li>
+					</ul>
+				</div>
+				<ul class="contact-list">
+					<li>
+						<a href="">
+							<img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/card.svg" alt="card" width="27">
+							<span>Carta dei servizi per il gioco a distanza</span>
+						</a>
+					</li>
+					<li><span>numero verde TVNGA</span><a class="number" href="tel:800558822">800 55 88 22</a></li>
+				</ul>
+			</div>
+			<div class="list-wrap">
+				<ul class="info-list">
+					<li class="title">Quote</li>
+					<li><a href="#">Quote Serie A</a></li>
+					<li><a href="#">Quote Serie B</a></li>
+					<li><a href="#">Quote Champions League</a></li>
+					<li><a href="#">Scommesse Oggi</a></li>
+				</ul>
+				<ul class="info-list">
+					<li class="title">Pronostici</li>
+					<li><a href="#">Pronostici Serie A</a></li>
+					<li><a href="#">Pronostici Serie B</a></li>
+					<li><a href="#">Pronostici Champions League</a></li>
+					<li><a href="#">Pronostici Oggi</a></li>
+				</ul>
+				<ul class="info-list">
+					<li class="title">Risorse Utili</li>
+					<li><a href="#">Guide Scommesse</a></li>
+					<li><a href="#">Glossario</a></li>
+					<li><a href="#">Statistiche Scommesse</a></li>
+					<li><a href="#">Risultati Live</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="copyright-block">
+			<div class="container">
+				<p>ASAP ITALIA SRL Piazza di Villa Carpegna 58, 00165 Roma P.Iva 10856401004 - Tutti i diritti riservati Superscommesse è una Testata Giornalistica registrata presso il Tribunale di Roma, n. 229/2013 del 09-10-2013 Societa' con socio unico soggetta a direzione e coordinamento di Catena Operations Ltd, Malta</p>
+			</div>
+		</div>
+	</footer>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -38,4 +92,5 @@ get_header();
 	</div><!-- .content-area -->
 
 <?php
+
 get_footer();
