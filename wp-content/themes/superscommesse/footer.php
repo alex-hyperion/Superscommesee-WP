b0152a0c39f19222e31c01a1a4786463db023717
julia-karpenko <julia.karpenko@catenamedia.com>
2019-12-11 11:04:31 +0200

    fixed footer

--- wp-content/themes/superscommesse/footer.php
+++ wp-content/themes/superscommesse/footer.php
@@ -7,39 +7,62 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
-		<div class="site-info">
-			<?php $blog_info = get_bloginfo( 'name' ); ?>
-			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
-			<?php endif; ?>
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'superscommesse' ) ); ?>" class="imprint">
-				<?php
-				/* translators: %s: WordPress. */
-				printf( __( 'Proudly powered by %s.', 'superscommesse' ), 'WordPress' );
-				?>
-			</a>
-			<?php
-			if ( function_exists( 'the_privacy_policy_link' ) ) {
-				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-			}
-			?>
-			<?php if ( has_nav_menu( 'footer' ) ) : ?>
-				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'superscommesse' ); ?>">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'menu_class'     => 'footer-menu',
-							'depth'          => 1,
-						)
-					);
-					?>
-				</nav><!-- .footer-navigation -->
-			<?php endif; ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="footer" id="footer">
+		<a class="to-top" href="#" id="js_to_top">Torna a inizio pagina</a>
+		<div class="footer-wrap">
+			<ul class="copyright-list">
+				<li><a href="#">Condizioni generali</a></li>
+				<li><a href="#">Privacy e cookie policy</a></li>
+			</ul>
+			<div class="info-wrap">
+				<div class="warning-text">
+					<p>il gioco è vietato ai minori di anni 18 - può causare dipendenza</p>
+					<ul class="icon-list">
+						<li><img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/age.svg" alt="18+" width="40"></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/legal.svg" alt="gioco legale e responsabile" width="70"></li>
+						<li><img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/aams.svg" alt="aams" width="49"></li>
+					</ul>
+				</div>
+				<ul class="contact-list">
+					<li>
+						<a href="">
+							<img src="<?php echo get_template_directory_uri()?>/assets/images/new-footer/card.svg" alt="card" width="27">
+							<span>Carta dei servizi per il gioco a distanza</span>
+						</a>
+					</li>
+					<li><span>numero verde TVNGA</span><a class="number" href="tel:800558822">800 55 88 22</a></li>
+				</ul>
+			</div>
+			<div class="list-wrap">
+				<ul class="info-list">
+					<li class="title">Quote</li>
+					<li><a href="#">Quote Serie A</a></li>
+					<li><a href="#">Quote Serie B</a></li>
+					<li><a href="#">Quote Champions League</a></li>
+					<li><a href="#">Scommesse Oggi</a></li>
+				</ul>
+				<ul class="info-list">
+					<li class="title">Pronostici</li>
+					<li><a href="#">Pronostici Serie A</a></li>
+					<li><a href="#">Pronostici Serie B</a></li>
+					<li><a href="#">Pronostici Champions League</a></li>
+					<li><a href="#">Pronostici Oggi</a></li>
+				</ul>
+				<ul class="info-list">
+					<li class="title">Risorse Utili</li>
+					<li><a href="#">Guide Scommesse</a></li>
+					<li><a href="#">Glossario</a></li>
+					<li><a href="#">Statistiche Scommesse</a></li>
+					<li><a href="#">Risultati Live</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="copyright-block">
+			<div class="container">
+				<p>ASAP ITALIA SRL Piazza di Villa Carpegna 58, 00165 Roma P.Iva 10856401004 - Tutti i diritti riservati Superscommesse è una Testata Giornalistica registrata presso il Tribunale di Roma, n. 229/2013 del 09-10-2013 Societa' con socio unico soggetta a direzione e coordinamento di Catena Operations Ltd, Malta</p>
+			</div>
+		</div>
+	</footer>
 
 </div><!-- #page -->
 
