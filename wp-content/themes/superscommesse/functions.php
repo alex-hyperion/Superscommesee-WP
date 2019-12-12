ea43a4e1ef3616db8550a987a7463ea40156ebb7
Alex <alexey.dyakonov@catenamedia.com>
2019-12-12 11:37:57 +0200

    Added options page for Header and footer

--- wp-content/themes/superscommesse/functions.php
+++ wp-content/themes/superscommesse/functions.php
@@ -7,3 +7,16 @@ if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
 	return;
 }
+
+// Check function exists.
+if( function_exists('acf_add_options_page') ) {
+
+	// Register options page.
+	$option_page = acf_add_options_page(array(
+		'page_title'    => __('Header&Footer Settings'),
+		'menu_title'    => __('Header&Footer Settings'),
+		'menu_slug'     => 'header-footer-settings',
+		'capability'    => 'edit_posts',
+		'redirect'      => false
+	));
+}
